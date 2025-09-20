8a3691df83e3b603632773d69482aa3bc3e60842
kiselkakakaka <klim200387@gmail.com>
2025-09-20 20:56:12 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/dal/FeedRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/FeedRepository.java
@@ -6,8 +6,13 @@ import ru.yandex.practicum.filmorate.model.feed.FeedEvent;
 import ru.yandex.practicum.filmorate.model.feed.Operation;
 
 public interface FeedRepository {
-    FeedEvent save(Long userId, EventType eventType, Operation operation, Long entityId, Long timestampMillis);
+
+    FeedEvent save(Long userId, EventType eventType, Operation operation,
+                   Long entityId, Long timestampMillis);
+
     List<FeedEvent> findByUserIdOrderByTimestamp(Long userId);
+
     List<FeedEvent> findFriendsFeedByUserIdOrderByTimestamp(Long userId);
+
     List<FeedEvent> findOwnAndFriendsFeedByUserIdOrderByTimestamp(Long userId);
 }
