f32bac2d091badc7e57220c68815b4eba02068cf
klimkiselev <klim200387@gmail.com>
2025-09-22 11:15:24 +0300

    add-feed fix

--- src/main/java/ru/yandex/practicum/filmorate/mapper/FeedEventMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/FeedEventMapper.java
@@ -6,7 +6,9 @@ import ru.yandex.practicum.filmorate.model.feed.FeedEvent;
 import java.util.List;
 
 public final class FeedEventMapper {
-    private FeedEventMapper() {}
+
+    private FeedEventMapper() {
+    }
 
     public static FeedEventDto toDto(FeedEvent e) {
         return new FeedEventDto(
@@ -20,6 +22,8 @@ public final class FeedEventMapper {
     }
 
     public static List<FeedEventDto> toDto(List<FeedEvent> events) {
-        return events.stream().map(FeedEventMapper::toDto).toList();
+        return events.stream()
+                .map(FeedEventMapper::toDto)
+                .toList();
     }
 }
