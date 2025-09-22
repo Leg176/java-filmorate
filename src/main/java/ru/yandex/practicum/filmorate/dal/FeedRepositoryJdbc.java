d6484aba0294784f97c26e827991f0f48c1aa311
klimkiselev <klim200387@gmail.com>
2025-09-22 11:06:33 +0300

    add-feed fix

--- src/main/java/ru/yandex/practicum/filmorate/dal/FeedRepositoryJdbc.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/FeedRepositoryJdbc.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.dal;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
