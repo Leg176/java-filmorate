1502d3032d41283b4db56310bd8d9c51ee0b0ab4
Романевский <V9139493364@yandex.ru>
2025-09-23 21:05:38 +0700

    Правка

--- src/main/java/ru/yandex/practicum/filmorate/controller/RecommendationController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/RecommendationController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.dto.film.FilmDto;
 import ru.yandex.practicum.filmorate.service.RecommendationService;
 
 import java.util.List;
@@ -22,7 +22,7 @@ public class RecommendationController {
     }
 
     @GetMapping("/{id}/recommendations")
-    public List<Film> getRecommendations(@PathVariable @Positive(message = "id должен быть больше 0") Long id) {
+    public List<FilmDto> getRecommendations(@PathVariable @Positive(message = "id должен быть больше 0") Long id) {
         return recommendationService.getRecommendations(id);
     }
 }
