a0ae62313c9b9999018779e72ff334183557ea43
fzk <fzk@fzk.fzk>
2025-09-24 18:21:10 +0200

    fix 404 bugs in get feed for unknown user

--- src/main/java/ru/yandex/practicum/filmorate/service/FeedService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FeedService.java
@@ -2,7 +2,9 @@ package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.dal.FeedRepository;
+import ru.yandex.practicum.filmorate.dal.UserRepository;
 import ru.yandex.practicum.filmorate.dto.event.EventResponseDto;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.mapper.EventMapper;
 
 import java.util.List;
@@ -11,12 +13,15 @@ import java.util.stream.Collectors;
 @Service
 public class FeedService {
     private final FeedRepository feedRepository;
+    private final UserRepository userRepository;
 
-    public FeedService(FeedRepository feedRepository) {
+    public FeedService(FeedRepository feedRepository, UserRepository userRepository) {
         this.feedRepository = feedRepository;
+        this.userRepository = userRepository;
     }
 
     public List<EventResponseDto> getFeed(Long id) {
+        userRepository.getUser(id).orElseThrow(() -> new NotFoundException("Пользователя с id: " + " не существует"));
         return feedRepository.getFeedForUser(id).stream().map(EventMapper::toDto).collect(Collectors.toList());
     }
 
