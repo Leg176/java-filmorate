fadb8246bc6ff728b05145f7efbc53bf11fc387b
Aleksei <shokolov@list.ru>
2025-09-25 17:11:08 +0300

    Мелкие недочёты

--- src/main/java/ru/yandex/practicum/filmorate/service/RecommendationService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/RecommendationService.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.dal.DirectorRepository;
 import ru.yandex.practicum.filmorate.dal.FilmRepository;
 import ru.yandex.practicum.filmorate.dal.GenreRepository;
 import ru.yandex.practicum.filmorate.dal.MpaRepository;
