c5ed9f9da67a994558d5cb99908d66707f001e83
Aleksei <shokolov@list.ru>
2025-09-26 21:42:40 +0300

    Оптимизирован запрос на вывод популярных фильмов, добавлена актуальная тема

--- src/main/java/ru/yandex/practicum/filmorate/service/RecommendationService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/RecommendationService.java
@@ -2,18 +2,18 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.dal.DirectorRepository;
 import ru.yandex.practicum.filmorate.dal.FilmRepository;
 import ru.yandex.practicum.filmorate.dal.GenreRepository;
 import ru.yandex.practicum.filmorate.dal.MpaRepository;
 import ru.yandex.practicum.filmorate.dto.film.FilmDto;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.mapper.FilmMapper;
+import ru.yandex.practicum.filmorate.model.Director;
+import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.MotionPictureAssociation;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,28 +22,28 @@ public class RecommendationService {
     private final FilmRepository filmRepository;
     private final MpaRepository mpaRepository;
     private final GenreRepository genreRepository;
+    private final DirectorRepository directorRepository;
 
     public RecommendationService(FilmRepository filmRepository, MpaRepository mpaRepository,
-                                 GenreRepository genreRepository) {
+                                 GenreRepository genreRepository, DirectorRepository directorRepository) {
         this.filmRepository = filmRepository;
         this.mpaRepository = mpaRepository;
         this.genreRepository = genreRepository;
+        this.directorRepository = directorRepository;
     }
 
-
     public List<FilmDto> getRecommendations(Long userId) {
-        return filmRepository.getRecommendations(userId).stream()
-                .map(id -> {
-                    List<Genre> genre = genreRepository.findGenresFilm(id.getIdFilm());
-                    Optional<MotionPictureAssociation> mpaOpt = mpaRepository.getMpa(id.getMpa().getId());
-                    if (mpaOpt.isEmpty()) {
-                        throw new NotFoundException("Mpa в базе данных не найдено");
-                    }
-                    MotionPictureAssociation mpa = mpaOpt.get();
-                    id.setMpa(mpa);
-                    id.setGenres(new HashSet<>(genre));
-                    return id;
-                })
+        List<Film> films = filmRepository.getRecommendations(userId);
+        List<Long> ids = films.stream().map(Film::getIdFilm).distinct().toList();
+        Map<Long, MotionPictureAssociation> mpaMap = mpaRepository.findMpaByFilmIds(ids);
+        Map<Long, Set<Genre>> genreMap = genreRepository.findGenresByFilmIds(ids);
+        Map<Long, Set<Director>> directorMap = directorRepository.findDirectorByFilmIds(ids);
+        for (Film f : films) {
+            f.setMpa(mpaMap.get(f.getIdFilm()));
+            f.setGenres(genreMap.getOrDefault(f.getIdFilm(), new HashSet<>()));
+            f.setDirector(directorMap.getOrDefault(f.getIdFilm(), new HashSet<>()));
+        }
+        return films.stream()
                 .map(FilmMapper::mapToFilmDto)
                 .collect(Collectors.toList());
     }
